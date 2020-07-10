bfec1d7bb0ea2ea93935408d8b6d45e07bb70d9f
Henry Liu <hl3184@columbia.edu>
2020-07-10 16:24:43 -0400

    modified style and element

--- script.js
+++ script.js
@@ -7,33 +7,40 @@ $(document).ready(() => {
 
   console.log(currentHour);
   for (let i = 9; i <= 17; i++) {
+    // conditional rendering according to current time
     const el = $(
-      `<div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text">${
-        i <= 12 ? i + 'AM' : i - 12 + 'PM'
-      }</span></div><textarea type="text" value="${
-        tasks[i] || ''
-      }"  class="form-control  ${
+      `<div class="row mb-1">
+      <div class="col-2">${i <= 12 ? i + 'AM' : i - 12 + 'PM'}</div>
+      <textarea type="text"   
+      class="form-control col-9  ${
         i === currentHour ? 'present' : i < currentHour ? 'past' : 'future'
-      }" data-key=${i} aria-label="Amount (to the nearest dollar)"/><div class="input-group-append saveBtn"><i class="fas fa-lock input-group-text" ></i></div></div>`
+      }" 
+      data-key=${i}>${tasks[i] || ''}</textarea>
+      <div class="col-1 saveBtn">
+      <i class="fas fa-lock icon fa-2x" ></i>
+      </div>
+      </div>`
     );
 
     $('.container').append(el);
   }
 
-  $('.input-group-append').on('click', (e) => {
-    // target the input box corrosponding to the save button
-    const input = e.target.parentElement.previousElementSibling;
+  $('.saveBtn').on('click', (e) => {
+    if (e.target.matches('i')) {
+      // target the textarea corrosponding to the save button
+      const input = e.target.parentElement.previousElementSibling;
 
-    // get the key of that input box
-    const key = parseInt(input.getAttribute('data-key'));
+      // get the key of that textarea box
+      const key = parseInt(input.getAttribute('data-key'));
 
-    // get the value of that input box
-    const value = input.value;
+      // get the value of that textarea box
+      const value = input.value;
 
-    // update the tasks array at that index
-    tasks[key] = value;
+      // update the tasks array at that index
+      tasks[key] = value;
 
-    // stored new tasks array to localstorage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+      // stored new tasks array to localstorage
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
   });
 });
